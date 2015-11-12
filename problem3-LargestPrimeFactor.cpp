22c4505ea1797507d8530b4ce57d8fc5f57b57ec
artem.kholodov <artemigkh@gmail.com>
2015-11-12 16:32:29 -0500

    Problem 3: Largest Prime Factor

---
+++ problem3-LargestPrimeFactor.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+/*
+ * this algorithm finds the largest prime divisor by removing all the previous prime factors
+ * with repeated division, so when the number of interest reaches 1 it will have to have
+ * been divided by the largest prime factor. That number is then printed.
+ */
+
+int main(){
+	long long int cn = 600851475143; //current number that we are finding prime factor of
+	long long int cd = 1; //current divisor
+	while(cn != 1){
+		cd++;
+		while(cn % cd == 0){
+			cn /= cd;
+		}		
+	}
+	std::cout << cd;
+}
