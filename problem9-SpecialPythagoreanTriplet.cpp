1292dbc32c89d70be72440bd6a0b9e78706bf3a6
Artem Kholodov <artemigkh@gmail.com>
2015-11-13 15:24:06 -0500

    Problem 9: Special Pythagorean Triplet
    Problem 9: Special Pythagorean Triplet

--- problem9-SpecialPythagoreanTriplet.cpp
+++ problem9-SpecialPythagoreanTriplet.cpp
@@ -7,8 +7,8 @@
  * In order to avoid many loop iterations, this problem will first be mathematically 
  * simplified. We start with 2 equations, (1) a^2 + b^2 = c^2 and (2) a + b + c = 1000.
  * We rearrange (1) to c = sqrt(a^2 + b^2) and sub into (2): a + b + sqrt(a^2 + b^2) = 1000.
- * Rearrange 2 in the form sqrt(a^2 + b^2) = 1000 - a -b and square both sides to obtain
- * a^2 + b^2 = a^2 + 2ab - 2000a + b^2 - 2000b + 1000000, then simplify this further to\
+ * Rearrange 2 in the form sqrt(a^2 + b^2) = 1000 - a - b and square both sides to obtain
+ * a^2 + b^2 = a^2 + 2ab - 2000a + b^2 - 2000b + 1000000, then simplify this further to
  * 0 = 2ab - 2000a - 2000b + 10000. Rearrange for a to get
  * a = (-2000b + 1000000) / (2000 - 2b).
  * Now that we have an equation for a in terms of b and an equation for c in terms of a and b
